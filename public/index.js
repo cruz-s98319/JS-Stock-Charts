696dd0edf3c166f58fdb74d589558ba497e2af29
Samantha Cruz <cruz.s98319@gmail.com>
2024-02-13 12:08:32 -0800

    Rough draft of for loop for fetch request to pass four stock symbols

--- public/index.js
+++ public/index.js
@@ -4,6 +4,17 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const apiKey = 'c1546ed75fdf49d0b3340f2bd265e885'
+    let symbol = ['GME', 'MSFT', 'DIS', 'BNTX'];
+
+    for (let x = 0; x < symbol.length; x++) {
+        let response = await fetch(`https://api.twelvedata.com/time_series?apikey=${apiKey}&interval=1day&symbol=${symbol[x]}&outputsize=4`);
+
+        let responseJSON = await response.json();
+
+        console.log(responseJSON);
+    }
+
 }
 
 main()
